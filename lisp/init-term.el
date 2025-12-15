5efc4d43531d18e2f87e3098ff00089a94dd1d41
Eason Sun <ballentin133@qq.com>
2025-12-15 22:30:54 +0800

    feat: 使用 eat

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -1,15 +1,7 @@
-;; 安装并配置 vterm（核心终端）
-(use-package vterm
+;; Better terminal emulator
+(use-package eat
   :ensure t
-  :commands vterm
-  :config
-  (setq vterm-max-scrollback 10000)) ; 可选：增大滚动缓冲区
-
-(defun my/vterm-new ()
-  "创建一个新的、独立命名的 vterm 终端缓冲区。"
-  (interactive)
-  ;; 生成唯一缓冲区名，例如 *vterm-<timestamp>*
-  (let ((buffer-name (generate-new-buffer-name "*vterm*")))
-    (vterm buffer-name)))
+  :hook ((eshell-load . eat-eshell-mode)
+         (eshell-load . eat-eshell-visual-command-mode)))
 
 (provide 'init-term)

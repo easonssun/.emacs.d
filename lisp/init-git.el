ac4116ec33ae7eb52718190b3231b5209b93d5d0
Eason Sun <ballentin133@qq.com>
2025-12-23 23:15:52 +0800

    feat: 修改 diff-hl 配置

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -16,8 +16,8 @@
 ;; Highlight uncommitted changes using VC
 (use-package diff-hl
   :ensure t
-  :bind (:map diff-hl-command-map
-         ("SPC" . diff-hl-mark-hunk))
+  ;; :bind (:map diff-hl-command-map
+  ;;        ("SPC" . diff-hl-mark-hunk))
   :hook ((after-init . global-diff-hl-mode)
          (after-init . global-diff-hl-show-hunk-mouse-mode)
          (dired-mode . diff-hl-dired-mode)
@@ -25,10 +25,11 @@
          (magit-post-refresh . diff-hl-magit-post-refresh))
   :custom
   (diff-hl-draw-borders nil)
-  ;; (diff-hl-update-async t)
+  (diff-hl-update-async t)
   (diff-hl-global-modes '(not image-mode pdf-view-mode))
   :init
   (setq diff-hl-show-staged-changes nil)
+  (setq diff-hl-reference-revision nil)
   :config
   ;; Set fringe style
   (setq-default fringes-outside-margins t)
@@ -40,9 +41,5 @@
 
   ;; Fall back to the display margin since the fringe is unavailable in tty
   (unless (display-graphic-p) (diff-hl-margin-mode 1)))
-
-;; 在 diff-hl 模式开启时启用自动刷新
-(add-hook 'diff-hl-mode-hook (lambda () (diff-hl-flydiff-mode 1)))
-
 
 (provide 'init-git)

c57088bd8e811b4e00417c8e8a765aa6411fe572
Eason Sun <ballentin133@qq.com>
2026-01-06 21:34:29 +0800

    feat: project 设置

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -16,4 +16,20 @@
   ;; 你可以在这里进行自定义配置，例如绑定快捷键
   )
 
+(defun my/find-project-root (&optional directory)
+  "向上查找包含特定标记文件的目录，作为项目根目录。
+默认查找 `.git` 或 `.projectile` 目录。"
+  (let ((dir (or directory default-directory)))
+    (or (locate-dominating-file dir ".git")
+        (locate-dominating-file dir ".projectile")
+        ;; 可以继续添加其他标记文件，如 'pom.xml', 'package.json' 等
+        dir))) ;; 如果找不到，返回当前目录
+
+(defun my/set-custom-root-as-default-directory ()
+  "将当前 buffer 的 default-directory 设置为自定义项目根目录。"
+  (interactive)
+  (setq-local default-directory (my/find-project-root)))
+
+(add-hook 'find-file-hook #'my/set-custom-root-as-default-directory)
+
 (provide 'init-project)
